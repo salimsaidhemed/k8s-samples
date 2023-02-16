8143cba55be731b5aa1454d58a5a52bc3df60fff
Salim Said Hemed <shemed@Salims-MacBook-Pro.local>
2023-02-16 17:52:28 +0300

    First Commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask,render_template,request
+app = Flask(__name__,static_url_path='/static')
+TITLE="k8s Sample Basic"
+PORT=5000
+@app.route("/")
+def index():
+    return render_template("index.html",title=TITLE)
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=PORT)
